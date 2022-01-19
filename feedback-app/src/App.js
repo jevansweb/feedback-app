c9f9e4c4c4c44c2675d4feb01474ec81a19ff358
Justin Evans <hsdjustin8@hotmail.com>
2022-01-19 15:00:48 +1000

    Changes to feedback App, pre react 5.0 update

---
+++ feedback-app/src/App.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackData from './data/FeedbackData'
+import FeedbackForm from './components/FeedbackForm'
+
+function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        console.log(newFeedback)
+    }
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }        
+    }
+
+    return (
+        <>
+        <Header />
+        <div className="container">
+            <FeedbackForm handleAdd={addFeedback}/>
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList feedback={feedback} 
+            handleDelete={deleteFeedback} />
+        </div>
+        </>
+    )
+}
+
+
+
+
+
+
+export default App
